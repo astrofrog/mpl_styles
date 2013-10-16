ebfdbbb76ecf5e4f989fb08a994778fe546f5720
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-10-16 18:53:40 +0200

    Simplify context manager

--- mpl_styles/__init__.py
+++ mpl_styles/__init__.py
@@ -1,8 +1,10 @@
+import sys
 import types
 import inspect
 
 from . import styles
 
+from matplotlib import rc_context, rcParams
 import matplotlib.pyplot as plt
 
 
@@ -13,15 +15,11 @@ def is_style_function(x):
 STYLE_FUNCTIONS = dict((name[6:], func) for name, func in inspect.getmembers(styles, predicate=is_style_function))
 
 
-class style(object):
-    def __enter__(self):
-        pass
-    def __exit__(self, ext, exv, trb):
-        for parameter, setting in self.previous.iteritems():
-            plt.rcParams[parameter] = setting
-    def __init__(self, name):
-        self.name = name
-        self.previous = {}
-        for parameter, setting in STYLE_FUNCTIONS[self.name]().iteritems():
-            self.previous[parameter] = plt.rcParams[parameter]
-            plt.rcParams[parameter] = setting
+def style(name):
+    context = rc_context()
+    try:
+        rcParams.update(STYLE_FUNCTIONS[name]())
+    except:
+        context.__exit__(*sys.exc_info())
+        raise
+    return context
